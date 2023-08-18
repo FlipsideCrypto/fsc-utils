fd800db3e05119d658f7c57492d9474bb8837686
drethereum <trevor.wenokur@gmail.com>
2023-08-18 12:49:06 -0600

    timestamp type

--- macros/python/generate_tbl_yml.py
+++ macros/python/generate_tbl_yml.py
@@ -47,7 +47,7 @@ def generate_yml(model_paths, output_dir=None, specific_files=[], drop_all=False
     column_test_mapping = {
         "STRING": "dbt_expectations.expect_column_values_to_match_regex:\n              regex: 0[xX][0-9a-fA-F]+\n",
         "INTEGER": "dbt_expectations.expect_column_values_to_be_in_type_list:\n              column_type_list:\n                - DECIMAL\n                - FLOAT\n                - NUMBER\n",
-        "TIMESTAMP": "dbt_expectations.expect_column_values_to_be_in_type_list:\n              column_type_list:\n                - TIMESTAMP_LTZ\n"
+        "TIMESTAMP": "dbt_expectations.expect_column_values_to_be_in_type_list:\n              column_type_list:\n                - TIMESTAMP_LTZ\n                - TIMESTAMP_NTZ\n"
     }
     for model_path in model_paths:
         specific_files = []
