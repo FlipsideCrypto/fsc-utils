0382613d49730873b4fc3624a7194a648533c81a
drethereum <trevor.wenokur@gmail.com>
2024-08-20 13:51:53 -0600

    decoder macro

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -91,7 +91,6 @@ WHERE
 {% macro streamline_external_table_query_v2(
         model,
         partition_function,
-        partition_column="partition_key",
         evm_balances=False
     ) %}
     WITH meta AS (
@@ -121,13 +120,13 @@ WHERE
             s
             JOIN meta b
             ON b.file_name = metadata$filename
-            AND b.partition_key = s.{{ partition_column }}
+            AND b.partition_key = s.partition_key
             {% if evm_balances %}
             JOIN {{ ref('_block_ranges') }} r
             ON r.block_number = COALESCE(s.VALUE :"BLOCK_NUMBER" :: INT,s.VALUE :"block_number" :: INT)
             {% endif %}
         WHERE
-            b.partition_key = s.{{ partition_column }}
+            b.partition_key = s.partition_key
             AND DATA :error IS NULL
             AND DATA IS NOT NULL
 {% endmacro %}
@@ -174,4 +173,90 @@ WHERE
     b.partition_key = s.{{ partition_column }}
     AND DATA :error IS NULL
     AND DATA IS NOT NULL
+{% endmacro %}
+
+{% macro streamline_external_table_query_decoder(
+        model
+    ) %}
+
+    WITH meta AS (
+
+        SELECT
+            job_created_time AS _inserted_timestamp,
+            file_name,
+            CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 6), '_', 1) AS INTEGER) AS _partition_by_block_number,
+            TO_DATE(
+                concat_ws('-', SPLIT_PART(file_name, '/', 3), SPLIT_PART(file_name, '/', 4), SPLIT_PART(file_name, '/', 5))
+            ) AS _partition_by_created_date
+        FROM
+            TABLE(
+                information_schema.external_table_file_registration_history(
+                    start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
+                    table_name => '{{ source( "bronze_streamline", model) }}')
+                ) A
+            )
+        SELECT
+            block_number,
+            id :: STRING AS id,
+            DATA,
+            _inserted_timestamp,
+            s._partition_by_block_number AS _partition_by_block_number,
+            s._partition_by_created_date AS _partition_by_created_date
+        FROM
+            {{ source(
+                "bronze_streamline",
+                model
+            ) }}
+            s
+            JOIN meta b
+            ON b.file_name = metadata$filename
+            AND b._partition_by_block_number = s._partition_by_block_number
+            AND b._partition_by_created_date = s._partition_by_created_date
+        WHERE
+            b._partition_by_block_number = s._partition_by_block_number
+            AND b._partition_by_created_date = s._partition_by_created_date
+            AND s._partition_by_created_date >= DATEADD('day', -2, CURRENT_TIMESTAMP())
+{% endmacro %}
+
+{% macro streamline_external_table_FR_query_decoder(
+        model
+    ) %}
+
+    WITH meta AS (
+
+        SELECT
+            registered_on AS _inserted_timestamp,
+            file_name,
+            CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 6), '_', 1) AS INTEGER) AS _partition_by_block_number,
+            TO_DATE(
+                concat_ws('-', SPLIT_PART(file_name, '/', 3), SPLIT_PART(file_name, '/', 4), SPLIT_PART(file_name, '/', 5))
+            ) AS _partition_by_created_date
+        FROM
+            TABLE(
+                information_schema.external_table_files(
+                    table_name => '{{ source( "bronze_streamline", model) }}'
+                )
+            ) A
+    )
+    SELECT
+        block_number,
+        id :: STRING AS id,
+        DATA,
+        _inserted_timestamp,
+        s._partition_by_block_number AS _partition_by_block_number,
+        s._partition_by_created_date AS _partition_by_created_date
+    FROM
+        {{ source(
+            "bronze_streamline",
+            model
+        ) }}
+        s
+        JOIN meta b
+        ON b.file_name = metadata$filename
+        AND b._partition_by_block_number = s._partition_by_block_number
+        AND b._partition_by_created_date = s._partition_by_created_date
+    WHERE
+        b._partition_by_block_number = s._partition_by_block_number
+        AND b._partition_by_created_date = s._partition_by_created_date
+        AND DATA NOT ILIKE '%"error":%'
 {% endmacro %}
